869c1d731c6b3fdf8b5d712451d2cccae832a05e
Peyton <pekellogg@hotmail.com>
2021-09-23 15:00:42 -0700

    commit tree 2f6f441a25731a5998ee460042057e118f61ddaf

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array = self.split
+    # binding.pry
+    count = 0
+    array.each do |word|
+      if word.sentence? || word.question? || word.exclamation?
+        count += 1
+      end
+    end
+    count
+    # binding.pry
   end
+
 end
+
+# complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+# String.new(complex_string)
+# complex_string.count_sentences #=> 4
